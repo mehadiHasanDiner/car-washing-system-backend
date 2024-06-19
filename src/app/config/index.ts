32fc33130b186699a25b7e59d6f2c4daabf73e20
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-06-19 18:32:31 +0600

    password hashed done

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -5,4 +5,5 @@ dotenv.config();
 export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
+  salt_round: process.env.BCRYPT_SALT_ROUNDS,
 };
--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,6 +1,8 @@
 import { Schema, model } from "mongoose";
 import { TUser } from "./user.interface";
 import { USER_Role } from "./user.constants";
+import bcryptjs from "bcryptjs";
+import config from "../../config";
 
 const userSchema = new Schema<TUser>({
   name: {
@@ -29,6 +31,17 @@ const userSchema = new Schema<TUser>({
     type: String,
     required: [true, "Address is required"],
   },
+});
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+  user.password = await bcryptjs.hash(user.password, Number(config.salt_round));
+  next();
+});
+
+userSchema.post("save", function (doc, next) {
+  doc.password = "";
+  next();
 });
 
 export const UserModel = model<TUser>("User", userSchema);
