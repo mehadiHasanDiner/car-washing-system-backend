e848a903772a4cfd75195d30e6856e34c02eea1a
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-09-18 01:18:59 +0600

    done

--- src/app/modules/review/review.service.ts
+++ src/app/modules/review/review.service.ts
@@ -6,8 +6,8 @@ export const getReviewFromDB = async (limit?: number, page?: number) => {
   const result = await ReviewModel.find()
     .populate("user")
     .sort({ createdAt: -1 })
-    .skip(currentPage * (limit || 2))
-    .limit(limit || 2);
+    // .skip(currentPage * (limit || 2))
+    // .limit(limit || 2);
   const totalDoc = await ReviewModel.countDocuments();
 
   return { totalDoc, result };
