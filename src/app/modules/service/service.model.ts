1c6a4d6271f132ad18b32c94defed80117399e93
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-09-19 06:53:14 +0600

    service page not done

--- src/app/modules/service/service.model.ts
+++ src/app/modules/service/service.model.ts
@@ -3,26 +3,14 @@ import { TService } from "./service.interface";
 
 const serviceSchema = new Schema<TService>(
   {
-    name: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    price: {
-      type: Number,
-      required: true,
-    },
-    duration: {
-      type: Number,
-      required: true,
-    },
-    isDeleted: {
-      type: Boolean,
-      default: false,
-    },
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    duration: { type: Number, required: true },
+    price: { type: Number, required: true },
+    image: { type: String, required: true },
+    rating: { type: Number, default: 0 },
+    featured: { type: Boolean, default: true },
+    isDeleted: { type: Boolean, required: false, default: false },
   },
   {
     timestamps: true,
--- src/app/modules/service/service.service.ts
+++ src/app/modules/service/service.service.ts
@@ -2,20 +2,94 @@ import httpStatus from "http-status";
 import AppError from "../../errors/AppErrors";
 import { ServiceModel } from "./service.model";
 import { TService } from "./service.interface";
+import { SlotModel } from "../slot/slot.model";
+import { Types } from "mongoose";
+
+type TSlot = {
+  _id: Types.ObjectId;
+  startTime: string;
+  endTime: string;
+  isBooked: string;
+};
+
+type TSlotData = {
+  date: string;
+  slots: TSlot[];
+};
 
 const createServiceIntoDB = async (payload: TService) => {
   const result = ServiceModel.create(payload);
   return result;
 };
 
-const getSingleServiceFromDB = async (id: string) => {
-  const result = ServiceModel.findById(id);
+const getAllServiceFromDB = async (query: Record<string, unknown>) => {
+  const filteredQuery = { ...query };
+  const removableKeys = ["name", "sort", "priceRange"];
+
+  removableKeys.forEach((key) => {
+    delete filteredQuery[key];
+  });
+
+  const filterQuery = ServiceModel.find(filteredQuery);
+  let name: string = "";
+  if (query?.name) {
+    name = query?.name as string;
+  }
+  const searchQuery = filterQuery.find({
+    name: { $regex: name, $options: "i" },
+  });
+
+  const priceRange = query?.priceRange
+    ? (query.priceRange as string).split(",")
+    : null;
+
+  const result = await searchQuery
+    .find({
+      ...(priceRange && {
+        price: {
+          $gte: Number(priceRange[0]),
+          $lte: Number(priceRange[1]),
+        },
+      }),
+      // Add other conditions here if needed
+    })
+    .sort((query?.sort as string) ? (query?.sort as string) : "-createdAt");
   return result;
 };
 
-const getAllServiceFromDB = async () => {
-  const result = await ServiceModel.find();
-  return result;
+const getSingleServiceFromDB = async (id: string) => {
+  const slots = await SlotModel.find({ service: id });
+
+  const transformedSlots: TSlotData[] = slots.reduce<TSlotData[]>(
+    (acc, current) => {
+      const existingEntry = acc.find((entry) => entry.date === current.date);
+
+      const slot: TSlot = {
+        _id: new Types.ObjectId(current._id),
+        startTime: current.startTime,
+        endTime: current.endTime,
+        isBooked: current.isBooked,
+      };
+
+      if (existingEntry) {
+        existingEntry.slots.push(slot);
+      } else {
+        acc.push({
+          date: current.date,
+          slots: [slot],
+        });
+      }
+
+      return acc;
+    },
+    []
+  );
+
+  const service = await Service.findById(id);
+  return {
+    service,
+    slots: transformedSlots,
+  };
 };
 
 const updateServiceIntoDB = async (id: string, payload: Partial<TService>) => {
