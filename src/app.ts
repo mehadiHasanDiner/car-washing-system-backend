fb50765380227b41329a0a4ab164d5ed78d07cab
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-10-02 02:28:27 +0600

    server js some edit

--- src/app.ts
+++ src/app.ts
@@ -8,14 +8,14 @@ const app: Application = express();
 
 // parser
 app.use(express.json());
-// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
-app.use(
-  cors({
-    origin: "https://peaceful-gumdrop-421df5.netlify.app",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://peaceful-gumdrop-421df5.netlify.app",
+//     credentials: true,
+//   })
+// );
 
 // test route
 const test = (req: Request, res: Response, next: NextFunction) => {
